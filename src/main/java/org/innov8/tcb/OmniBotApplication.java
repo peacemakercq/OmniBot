1c5a49bbaa78648c7bc8276132e3f508d27fd413
peacemakercq <qiancheng40@live.cn>
2019-09-02 23:33:00 +0800

    Scaffold empty Spring Boot project
    ...with Aliyun repository

---
+++ src/main/java/org/innov8/tcb/OmniBotApplication.java
@@ -0,0 +1,13 @@
+package org.innov8.tcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniBotApplication.class, args);
+	}
+
+}
